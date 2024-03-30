469edb2a06f51d662a9196c50859f9e9a86aec0d
Anthony Araye <anthony.araye@akur8.com>
2024-03-30 23:40:24 +0100

    [FRONT] Home page (without templates for languages)

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { GridUtilsService } from '../services/grid-utils.service';
+import { GridComponent, GridConfig } from '../components/grid/grid.component';
+import { MatList, MatListItem, MatListItemLine, MatListItemTitle } from '@angular/material/list';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  standalone: true
+  standalone: true,
+  imports: [
+    MatCardModule,
+    GridComponent,
+    MatList,
+    MatListItem,
+    MatListItemLine,
+    MatListItemTitle,
+  ]
 })
 export class HomeComponent {
+  @HostBinding("class.w-full") wFull = true;
+  @HostBinding("class.h-full") hFull = true;
+
+  gridConfigExample: GridConfig = {
+    colors: this.gridUtilsService.generateColors(5)
+  };
+
+  constructor(private gridUtilsService: GridUtilsService) {}
 }
